ca5ae92a0b87be04b3f832711f9fdd9bfd57e700
vijay15-coder <angajalavijay8560@gmail.com>
2025-12-12 13:34:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
         "*",
-        "https://matchmaking-u9a9.vercel.app"
+        "https://match-5b62.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
